15edd0bc134c15f0e25b0ab33a1c660194f13d93
kymkbllr <billur2756@gmail.comssh keygen -t rsa -C billur2756@gmail.comgit config --global user.email billur2756@gmail.com>
2017-02-28 23:08:35 +0300

    ilk kurulum

---
+++ app/src/main/java/com/coverevi/coverevi/MainActivity.java
@@ -0,0 +1,15 @@
+package com.coverevi.coverevi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // TODO: GridView screen genişliğine göre otomatik ayarlanacak
+    }
+}
