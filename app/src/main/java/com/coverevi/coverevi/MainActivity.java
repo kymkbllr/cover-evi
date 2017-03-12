af6ebb888940c3ee6bc6e7e13668320b314e08ef
kymkbllr <billur2756@gmail.comssh keygen -t rsa -C billur2756@gmail.comgit config --global user.email billur2756@gmail.com>
2017-03-12 20:48:27 +0300

    //navigasyon drawer oluşturuldu.

--- app/src/main/java/com/coverevi/coverevi/MainActivity.java
+++ app/src/main/java/com/coverevi/coverevi/MainActivity.java
@@ -1,13 +1,23 @@
 package com.coverevi.coverevi;
 
+import android.content.res.Configuration;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.GridView;
 import android.widget.HorizontalScrollView;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView listView;
+    private NavigationDrawer adapter;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +29,17 @@ public class MainActivity extends AppCompatActivity {
         HaberAdapter haberAdapter = new HaberAdapter(this );
         gridView.setAdapter(haberAdapter);
 
+        listView = (ListView) findViewById(R.id.navList);
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+
+        addDrawerItems();
+        setupDrawer();
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+
 
         LinearLayoutManager layoutManager
                 = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
@@ -29,5 +50,60 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(youtubeAdapter);
 
 
+    }
+
+    private void setupDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        drawerLayout.addDrawerListener(mDrawerToggle);
+    }
+
+    private void addDrawerItems() {
+        adapter = new NavigationDrawer(this);
+        listView.setAdapter(adapter);
+
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        // Activate the navigation drawer toggle
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
