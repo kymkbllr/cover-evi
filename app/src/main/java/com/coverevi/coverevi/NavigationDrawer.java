0179c8ae7d78da84ab22c399dcfa827d3713f843
kymkbllr <billur2756@gmail.comssh keygen -t rsa -C billur2756@gmail.comgit config --global user.email billur2756@gmail.com>
2017-03-12 21:35:48 +0300

    //nav item oluşturuldu. //adapter yazıldı.

--- app/src/main/java/com/coverevi/coverevi/NavigationDrawer.java
+++ app/src/main/java/com/coverevi/coverevi/NavigationDrawer.java
@@ -1,21 +1,32 @@
 package com.coverevi.coverevi;
 
 import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 
-/**
- * Created by kymkbllr on 12.03.2017.
- */
 public class NavigationDrawer extends BaseAdapter{
 
+    private LayoutInflater layoutInflater;
+    private String [] menuogeleri;
+
     public NavigationDrawer(Activity activity) {
+        layoutInflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        menuogeleri= new String[] {
+                "anasayfa",
+                "hakkımızda",
+                "iletişim",
+                "çekimlerimiz",
+                "etkinlikler"
+        };
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return menuogeleri.length;
     }
 
     @Override
@@ -30,6 +41,12 @@ public class NavigationDrawer extends BaseAdapter{
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        View view = layoutInflater.inflate(R.layout.nav_item, null);
+
+        Button btn = (Button) view.findViewById(R.id.nav_item);
+        btn.setText(menuogeleri[position]);
+
+        return view;
+
     }
 }
