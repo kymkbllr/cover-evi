bce76660acc7725a3654d4a0e364d586a343f90c
Emre Aydin <emreaydin149@gmail.com>
2017-03-15 14:36:01 +0300

    action bar resim eklendi.

--- app/src/main/java/com/coverevi/coverevi/MainActivity.java
+++ app/src/main/java/com/coverevi/coverevi/MainActivity.java
@@ -1,22 +1,21 @@
 package com.coverevi.coverevi;
 
+import android.app.ActionBar;
 import android.content.res.Configuration;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.GridView;
-import android.widget.HorizontalScrollView;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
     private ListView listView;
-    private NavigationDrawer adapter;
     private ActionBarDrawerToggle mDrawerToggle;
     private DrawerLayout drawerLayout;
 
@@ -32,28 +31,29 @@ public class MainActivity extends AppCompatActivity {
         addDrawerItems();
         setupDrawer();
 
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
-
-        LinearLayoutManager layoutManager
-                = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
-
-        // Burada ilk açılışta ekran gelir.
-        View view = getLayoutInflater().inflate(R.layout.anasayfa, null);
-
-        LinearLayout ana_layout = (LinearLayout) findViewById(R.id.ana_icerik);
-        ana_layout.addView(view);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setHomeButtonEnabled(true);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
 
         // TODO: GridView screen genişliğine göre otomatik ayarlanacak
         GridView gridView = (GridView) findViewById(R.id.gdHaber);
         HaberAdapter haberAdapter = new HaberAdapter(this );
         gridView.setAdapter(haberAdapter);
 
+        LinearLayoutManager layoutManager
+                = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rcYoutube);
         recyclerView.setLayoutManager(layoutManager);
         YoutubeAdapter youtubeAdapter = new YoutubeAdapter(this);
         recyclerView.setAdapter(youtubeAdapter);
+
+        final android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setCustomView(R.layout.action_bar_header);
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setBackgroundDrawable(new ColorDrawable(0xff1f1d20));
 
     }
 
@@ -78,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void addDrawerItems() {
-        adapter = new NavigationDrawer(this);
+        NavigationDrawer adapter = new NavigationDrawer(this);
         listView.setAdapter(adapter);
 
     }
@@ -101,14 +101,11 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        // int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
         // Activate the navigation drawer toggle
-        if (mDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
+        return mDrawerToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
 
-        return super.onOptionsItemSelected(item);
     }
 }
